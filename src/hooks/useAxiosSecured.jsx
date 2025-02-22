9c9bb9f72fe5e68ae46f7d745e8be13b29237320
web-mahadihasan <mehedihasanmilu7@gmail.com>
2025-02-22 19:20:29 +0600

    solve vercel reload issue

--- src/hooks/useAxiosSecured.jsx
+++ src/hooks/useAxiosSecured.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import { useEffect } from "react";
 
 export const axiosSecured = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://app-taskszen-server.vercel.app",
 });
 
 const useAxiosSecured = () => {
